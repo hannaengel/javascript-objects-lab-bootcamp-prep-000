02db538bd8762015d468ce97a08bde077f8bc8b6
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-30 21:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var recipes = {ingredient1: 'eggs', ingredient2:'butter', ingredient3: 'milk'}
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[kay] = value
+  object[key] = value
   return object
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  newObject = Object.assign({}, object, key:value)
+  return newObject
 }
