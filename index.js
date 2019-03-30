5803017bf1349316c3a4b71e70b93357f42954d1
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-30 21:25:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return object[key] = value
+  var object =  object[key] = value;
+   return object
 
 }
 function deleteFromObjectByKey(object, key){
