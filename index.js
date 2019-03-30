938dbf0226d30d48150769d890c7e01c59e744ca
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-30 21:07:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var recipes = {ingredient1: 'eggs', ingredient2:'butter', ingredient3: 'milk'}
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[kay] = value
+  return object
+}
